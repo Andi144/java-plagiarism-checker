8d2d6c38ddc74a5b086620ed03cca16c3333c627
Andreas Schörgenhumer <andischoe@gmail.com>
2023-01-05 16:17:54 +0100

    Initial commit

---
+++ src/main/java/ASTRenamer.java
@@ -0,0 +1,170 @@
+import spoon.Launcher;
+import spoon.refactoring.CtRenameGenericVariableRefactoring;
+import spoon.refactoring.Refactoring;
+import spoon.reflect.CtModel;
+import spoon.reflect.code.CtLocalVariable;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class ASTRenamer {
+	
+	private static class RenamingData {
+		
+		final boolean includeCount;
+		final boolean countGlobally;
+		private final String template;
+		private int count;
+		
+		RenamingData(boolean includeCount, boolean countGlobally, String template) {
+			this.includeCount = includeCount;
+			this.countGlobally = countGlobally;
+			this.template = template;
+			count = 0;
+		}
+		
+		String createName() {
+			String name = template;
+			if (includeCount) {
+				name += count;
+				count++;
+			}
+			return name;
+		}
+		
+		void resetCount() {
+			count = 0;
+		}
+		
+	}
+	
+	private final Launcher launcher;
+	private final CtModel model;
+	private final RenamingData typeData;
+	private final RenamingData fieldData;
+	private final RenamingData methodData;
+	private final RenamingData parameterData;
+	private final RenamingData localVariableData;
+	
+	public ASTRenamer(String path) {
+		this(path, true);
+	}
+	
+	public ASTRenamer(String path, boolean keepComments) {
+		this(path, keepComments, true, false);
+	}
+	
+	public ASTRenamer(String path, boolean keepComments, boolean includeCount) {
+		this(path, keepComments, includeCount, false);
+	}
+	
+	public ASTRenamer(String path, boolean keepComments, boolean includeCount, boolean countGlobally) {
+		this(path, keepComments,
+				includeCount, includeCount, includeCount, includeCount, includeCount,
+				countGlobally, countGlobally, countGlobally, countGlobally, countGlobally,
+				"__type__", "__field__", "__method__", "__parameter__", "__localVariable__");
+	}
+	
+	// TODO: maybe put "path" into "rename" method, so the same renaming settings can be used for multiple files, and
+	//  then return the model in "rename"; also, this would mean that we lose the state, so methods like "toString" and
+	//  "getTopLevelTypes" would not make sense afterwards anymore (ASTRenamer is stateless w.r.t. to the renamed models)
+	public ASTRenamer(
+			String path, boolean keepComments,
+			boolean includeTypeCount, boolean includeFieldCount, boolean includeMethodCount, boolean includeParameterCount, boolean includeLocalVariableCount,
+			boolean countTypesGlobally, boolean countFieldsGlobally, boolean countMethodsGlobally, boolean countParametersGlobally, boolean countLocalVariablesGlobally,
+			String typeTemplate, String fieldTemplate, String methodTemplate, String parameterTemplate, String localVariableTemplate
+	) {
+		this.launcher = new Launcher();
+		// path can be a folder or a file
+		// addInputResource can be called several times
+		launcher.addInputResource(path);
+		// the compliance level should be set to the java version targeted by the input resources, e.g. Java 17
+		launcher.getEnvironment().setComplianceLevel(17);
+		launcher.getEnvironment().setCommentEnabled(keepComments);
+		launcher.buildModel();
+		model = launcher.getModel();
+		
+		typeData = new RenamingData(includeTypeCount, countTypesGlobally, typeTemplate);
+		fieldData = new RenamingData(includeFieldCount, countFieldsGlobally, fieldTemplate);
+		methodData = new RenamingData(includeMethodCount, countMethodsGlobally, methodTemplate);
+		parameterData = new RenamingData(includeParameterCount, countParametersGlobally, parameterTemplate);
+		localVariableData = new RenamingData(includeLocalVariableCount, countLocalVariablesGlobally, localVariableTemplate);
+	}
+	
+	// TODO: temp!
+	public CtType<?> renameType(CtType<?> type) {
+		CtType<?> copiedType = type.clone(); //Refactoring.copyType(type);
+		rename(List.of(copiedType), typeData, (ctType, newName) -> {
+			Refactoring.changeTypeName(ctType, newName);
+			renameTypeInternals(ctType);
+		});
+		return copiedType;
+	}
+	
+	public void rename() {
+		// "model.getAllTypes()" only identified top-level types (not nested types), so filter by type
+		rename(model.getElements(new TypeFilter<>(CtType.class)), typeData, (ctType, newName) -> {
+			Refactoring.changeTypeName(ctType, newName);
+			renameTypeInternals(ctType);
+		});
+	}
+	
+	private void renameTypeInternals(CtType<?> ctType) {
+		rename(ctType.getFields(), fieldData, (ctField, newName) -> new CtRenameGenericVariableRefactoring().setTarget(ctField).setNewName(newName).refactor());
+		rename(ctType.getMethods(), methodData, (ctMethod, newName) -> {
+			Refactoring.changeMethodName(ctMethod, newName);
+			renameMethodInternals(ctMethod);
+		});
+	}
+	
+	private void renameMethodInternals(CtMethod<?> ctMethod) {
+		rename(ctMethod.getParameters(), parameterData, (ctParameter, newName) -> new CtRenameGenericVariableRefactoring().setTarget(ctParameter).setNewName(newName).refactor());
+		rename(ctMethod.getElements(new TypeFilter<>(CtLocalVariable.class)), localVariableData, (ctLocalVariable, newName) -> {
+			// this performs a check for valid renaming
+//			Refactoring.changeLocalVariableName(ctLocalVariable, newName);
+			// this does not perform a check for valid renaming
+			new CtRenameGenericVariableRefactoring().setTarget(ctLocalVariable).setNewName(newName).refactor();
+		});
+	}
+	
+	private <E> void rename(Collection<E> elements, RenamingData data, BiConsumer<E, String> renamer) {
+		for (E e : elements) {
+			renamer.accept(e, data.createName());
+		}
+		if (!data.countGlobally) {
+			data.resetCount();
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (CtType<?> ctType : model.getAllTypes()) {
+			sb.append(ctType.toString());
+			sb.append("\n\n");
+		}
+		return sb.toString();
+	}
+	
+	public List<CtType<?>> getTopLevelTypes() {
+		// TODO: stream?? model.getAllTypes().stream().map(CtType::clone).toList() does not work
+		List<CtType<?>> copies = new ArrayList<>();
+		model.getAllTypes().forEach(c -> copies.add(c.clone()));
+		return Collections.unmodifiableList(copies);
+	}
+	
+	public void writeToFile() {
+		// TODO: parameterize
+		// TODO: maybe do not even use "launcher" but just do everything manually like:
+		//   for (CtType<?> ctType : model.getAllTypes()) {
+		//       write ctType.toString to file (with same name as this ctType)
+		launcher.prettyprint();
+	}
+	
+}
---
+++ src/main/java/Application.java
@@ -0,0 +1,240 @@
+import gumtree.spoon.AstComparator;
+import gumtree.spoon.diff.Diff;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.text.similarity.JaccardSimilarity;
+import org.apache.commons.text.similarity.JaroWinklerSimilarity;
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtType;
+
+import java.util.*;
+import java.util.function.Function;
+
+public class Application {
+	
+	// TODO: should be an arg
+	// if avg metric is < threshold --> issue plagiarism warning
+	// TODO: very simple heuristic, maybe change into something more sophisticated (e.g., via exchangeable interface
+	//  implementation (strategy pattern) "interface PlagDetection { boolean isPlag(Map<String, Double> metrics) }")
+	private static final double THRESHOLD = 0.05;
+	
+	private static <E> E extractArg(String[] args, String arg, Function<String, E> transformer, E defaultVal) {
+		// TODO: assumes arg-pairs: [arg_name_1, arg_value_1, ..., arg_name_n, arg_value_n]
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals(arg)) {
+				return transformer.apply(args[i + 1]);
+			}
+		}
+		if (defaultVal != null) {
+			return defaultVal;
+		}
+		throw new IllegalArgumentException("Argument " + arg + " could not be found");
+	}
+	
+	private static <E> List<E> extractListArg(String[] args, String arg, Function<String, E> transformer) {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals(arg)) {
+				// assume all following args are list values until we encounter "-XYZ" or "--XYZ", i.e., a new argument
+				List<E> list = new ArrayList<>();
+				for (int j = i + 1; j < args.length && !args[j].startsWith("-") && !args[j].startsWith("--"); j++) {
+					list.add(transformer.apply(args[j]));
+				}
+				return Collections.unmodifiableList(list);
+			}
+		}
+		throw new IllegalArgumentException("Argument " + arg + " could not be found");
+	}
+	
+	public static void main(String[] args) {
+		List<String> folders = extractListArg(args, "--folders", Function.identity());
+		int verbosity = extractArg(args, "--verbosity", Integer::parseInt, 0);
+		// TODO: parallelize
+		for (int i = 0; i < folders.size() - 1; i++) {
+			for (int j = i + 1; j < folders.size(); j++) {
+				compareFolders(folders.get(i), folders.get(j), verbosity);
+			}
+		}
+	}
+	
+	// TODO: replace verbosity and standard output printing with logging
+	private static void compareFolders(String folder1, String folder2, int verbosity) {
+		if (verbosity >= 1) {
+			System.out.println("##########################################################");
+			System.out.println("Comparing folder");
+			System.out.println(folder1);
+			System.out.println("to");
+			System.out.println(folder2);
+			System.out.println("##########################################################");
+		}
+		
+		ASTRenamer renamer1 = new ASTRenamer(folder1, false, false);
+		ASTRenamer renamer2 = new ASTRenamer(folder2, false, false);
+//		renamer1.rename();
+//		renamer2.rename();
+		List<CtType<?>> types1 = renamer1.getTopLevelTypes();
+		List<CtType<?>> types2 = renamer2.getTopLevelTypes();
+		
+		AstComparator comparator = new AstComparator();
+		for (CtType<?> type1 : types1) {
+			// TODO: hard-coded, should be parameterized via blacklist, e.g., "excludedTypeNames" and then
+			//  excludedTypeNames.stream().anyMatch(x -> x.equals(type.getSimpleName()))
+			if (type1.getSimpleName().equals("In") || type1.getSimpleName().equals("Out")) {
+				continue;
+			}
+			
+			int count1 = countElements(type1);
+			CtType<?> renamedType1 = renamer1.renameType(type1);
+			
+			Map<CtType<?>, List<Integer>> comparison = new HashMap<>();
+			ArrayList<Pair<CtType<?>, Integer>> typeDiffSizes = new ArrayList<>();
+			ArrayList<Pair<CtType<?>, Integer>> renamedTypeDiffSizes = new ArrayList<>();
+			ArrayList<Pair<CtType<?>, Integer>> countDiffs = new ArrayList<>();
+			ArrayList<Pair<CtType<?>, Integer>> levenDiffs = new ArrayList<>();
+			
+			if (verbosity >= 2) {
+				System.out.println("Comparing '" + type1.getSimpleName() + "'");
+			}
+			for (CtType<?> type2 : types2) {
+				// TODO: hard-coded, should be parameterized via blacklist, e.g., "excludedTypeNames" and then
+				//  excludedTypeNames.stream().anyMatch(x -> x.equals(type.getSimpleName()))
+				if (type2.getSimpleName().equals("In") || type2.getSimpleName().equals("Out")) {
+					continue;
+				}
+				
+				comparison.put(type2, new ArrayList<>());
+				
+				Diff typeDiff = comparator.compare(type1, type2);
+				int typeDiffSize = typeDiff.getRootOperations().size();
+				typeDiffSizes.add(Pair.of(type2, typeDiffSize));
+				
+				CtType<?> renamedType2 = renamer2.renameType(type2);
+				Diff renamedTypeDiff = comparator.compare(renamedType1, renamedType2);
+				int renamedTypeDiffSize = renamedTypeDiff.getRootOperations().size();
+				renamedTypeDiffSizes.add(Pair.of(type2, renamedTypeDiffSize));
+				
+				int count2 = countElements(type2);
+				int countDiff = Math.abs(count1 - count2);
+				countDiffs.add(Pair.of(type2, countDiff));
+				
+				int levenDiff = new LevenshteinDistance().apply(type1.getSimpleName(), type2.getSimpleName());
+				levenDiffs.add(Pair.of(type2, levenDiff));
+				
+				if (verbosity >= 2) {
+					System.out.format("  to %25s: typeDiffSize = %3d, renamedTypeDiffSize = %3d, countDiff = %4d, levenDiff = %3d\n", "'" + type2.getSimpleName() + "'", typeDiffSize, renamedTypeDiffSize, countDiff, levenDiff);
+				}
+			}
+			
+			// TODO: comments do not include levenDiff
+			// For each diff list, sort according to the minimum diff and then assign values 1, 2, 3, ... to the
+			// respective AST elements (the various "type2" from above). Afterwards, for each AST element (for each
+			// "type2" from above), we have a normalized sorting. Example:
+			// Comparing 'Exercise1' ("type1")
+			//   to OneDimensionalArray ("type2"), typeDiffSize = 55, renamedTypeDiffSize = 52, countDiff =  11
+			//   to      StoryGenerator ("type2"), typeDiffSize = 70, renamedTypeDiffSize = 63, countDiff =  36
+			//   to      TextConversion ("type2"), typeDiffSize = 58, renamedTypeDiffSize = 59, countDiff =  46
+			//   to       TwoDimensions ("type2"), typeDiffSize = 93, renamedTypeDiffSize = 97, countDiff = 284
+			// This will be mapped to:
+			// Comparing 'Exercise1' ("type1")
+			//   to OneDimensionalArray ("type2"), typeDiffSize = 1, renamedTypeDiffSize = 1, countDiff = 1
+			//   to      StoryGenerator ("type2"), typeDiffSize = 3, renamedTypeDiffSize = 3, countDiff = 2
+			//   to      TextConversion ("type2"), typeDiffSize = 2, renamedTypeDiffSize = 2, countDiff = 3
+			//   to       TwoDimensions ("type2"), typeDiffSize = 4, renamedTypeDiffSize = 4, countDiff = 4
+			sortAndAddNormalized(typeDiffSizes, comparison);
+			sortAndAddNormalized(renamedTypeDiffSizes, comparison);
+			sortAndAddNormalized(countDiffs, comparison);
+			sortAndAddNormalized(levenDiffs, comparison);
+			
+			// With the new, normalized mapping from above, we can now look for the "best" match, where "best" is simply
+			// defined as the minimum product of an AST element's (a "type2"'s) differences. The product (compared to
+			// summing the differences) has the advantage that multiple low values are more heavily rewarded, e.g., if
+			// there are the diffs (1, 1, 4) and (2, 2, 1), the product (4 vs. 9) would still favor the first diff,
+			// whereas the sum (8 vs. 7) would favor the second diff, which probably follows the intuition that the
+			// first diff is likely the matching one ("two best scores vs. probably one outlier"). Of course, this is
+			// just a heuristic and can lead to false results. Example from above:
+			// Comparing 'Exercise1' ("type1")
+			//   to OneDimensionalArray ("type2"), typeDiffSize = 1, renamedTypeDiffSize = 1, countDiff = 1 --> product =  1
+			//   to      StoryGenerator ("type2"), typeDiffSize = 3, renamedTypeDiffSize = 3, countDiff = 2 --> product = 18
+			//   to      TextConversion ("type2"), typeDiffSize = 2, renamedTypeDiffSize = 2, countDiff = 3 --> product = 12
+			//   to       TwoDimensions ("type2"), typeDiffSize = 4, renamedTypeDiffSize = 4, countDiff = 4 --> product = 64
+			// In this case, the 'OneDimensionalArray' AST element ("type2") will be selected as the best match for the
+			// original AST element 'Exercise1' ("type1").
+			CtType<?> matchingType = comparison.entrySet().stream()
+					.map(entry -> Pair.of(entry.getKey(), entry.getValue().stream()
+							.mapToInt(x -> x)
+							.reduce(1, (a, x) -> a * x)))
+					.min(Comparator.comparingInt(Pair::getRight))
+					.map(Pair::getLeft)
+					.orElseThrow();
+			
+			Map<String, Double> metrics = computeMetrics(type1, matchingType, renamedType1, renamer2.renameType(matchingType));
+			double avg = metrics.values().stream().mapToDouble(x -> x).average().orElseThrow();
+			System.out.printf("average = %.3f\n", avg);
+			if (avg < THRESHOLD) {
+				if (verbosity >= 1) {
+					System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					System.out.printf("average (%.3f) is < threshold (%.3f)\n", avg, THRESHOLD);
+					System.out.println("folder1: " + folder1);
+					System.out.println("folder2: " + folder2);
+					System.out.println("type1: " + type1.getSimpleName());
+					System.out.println("type2: " + matchingType.getSimpleName());
+					System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					System.out.println();
+				}
+			}
+		}
+	}
+	
+	private static void sortAndAddNormalized(ArrayList<Pair<CtType<?>, Integer>> diffs, Map<CtType<?>, List<Integer>> comparison) {
+		diffs.sort(Comparator.comparingInt(Pair::getRight));
+		int i = 1;
+		for (Pair<CtType<?>, Integer> pair : diffs) {
+			comparison.get(pair.getLeft()).add(i);
+			i++;
+		}
+	}
+	
+	private static int countElements(CtElement element) {
+		int count = 1;
+		for (CtElement e : element.getDirectChildren()) {
+			count += countElements(e);
+		}
+		return count;
+	}
+	
+	// TODO: currently just printing without verbosity check
+	private static Map<String, Double> computeMetrics(CtType<?> type1, CtType<?> type2, CtType<?> renamedType1, CtType<?> renamedType2) {
+		AstComparator comparator = new AstComparator();
+		System.out.println("Comparing '" + type1.getSimpleName() + "' to " + "'" + type2.getSimpleName() + "'");
+		Diff typeDiff = comparator.compare(type1, type2);
+		int typeDiffSize = typeDiff.getRootOperations().size();
+		System.out.println("AST changes (before renaming): " + typeDiffSize);
+		Diff renamedTypeDiff = comparator.compare(renamedType1, renamedType2);
+		int renamedTypeDiffSize = renamedTypeDiff.getRootOperations().size();
+		System.out.println("AST changes  (after renaming): " + renamedTypeDiffSize);
+		int count1 = countElements(type1);
+		int count2 = countElements(type2);
+		int countDiff = Math.abs(count1 - count2);
+		System.out.println("AST element count difference: " + countDiff);
+		
+		double typeDiffSizeMetric = (double) typeDiffSize / Math.max(count1, count2);
+		double renamedDiffSizeMetric = (double) renamedTypeDiffSize / Math.max(count1, count2);
+		double countDiffMetric = (double) countDiff / Math.max(count1, count2);
+		double jaccard = 1 - new JaccardSimilarity().apply(type1.toString(), type2.toString());
+		double renamedJaccard = 1 - new JaccardSimilarity().apply(renamedType1.toString(), renamedType2.toString());
+		double jaro = 1 - new JaroWinklerSimilarity().apply(type1.toString(), type2.toString());
+		double renamedJaro = 1 - new JaroWinklerSimilarity().apply(renamedType1.toString(), renamedType2.toString());
+		
+		Map<String, Double> metrics = Map.of(
+				"typeDiffSizeMetric", typeDiffSizeMetric,
+				"renamedDiffSizeMetric", renamedDiffSizeMetric,
+				"countDiffMetric", countDiffMetric,
+				"jaccard", jaccard,
+				"renamedJaccard", renamedJaccard,
+				"jaro", jaro,
+				"renamedJaro", renamedJaro);
+		metrics.forEach((key, value) -> System.out.printf("%21s: %.3f\n", key, value));
+		System.out.println();
+		return metrics;
+	}
+	
+}
