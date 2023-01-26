9ac7d5764d9b1d4331124ea122ead52052c71b7e
Andreas Schörgenhumer <andischoe@gmail.com>
2023-01-26 15:21:34 +0100

    Extract type matching into separate method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -80,8 +80,9 @@ public class Application {
 //		renamer2.rename();
 		List<CtType<?>> types1 = renamer1.getTopLevelTypes();
 		List<CtType<?>> types2 = renamer2.getTopLevelTypes();
+		// TODO: remove excluded type names already here, so we do not unnecessarily iterate through them in the for loops
 		
-		AstComparator comparator = new AstComparator();
+		
 		FolderComparison folderComparison = new FolderComparison(folder1, folder2);
 		
 		for (CtType<?> type1 : types1) {
@@ -91,95 +92,100 @@ public class Application {
 				continue;
 			}
 			
-			int count1 = countElements(type1);
-			CtType<?> renamedType1 = renamer1.renameType(type1);
-			
-			Map<CtType<?>, List<Integer>> comparison = new HashMap<>();
-			ArrayList<Pair<CtType<?>, Integer>> typeDiffSizes = new ArrayList<>();
-			ArrayList<Pair<CtType<?>, Integer>> renamedTypeDiffSizes = new ArrayList<>();
-			ArrayList<Pair<CtType<?>, Integer>> countDiffs = new ArrayList<>();
-			ArrayList<Pair<CtType<?>, Integer>> levenDiffs = new ArrayList<>();
-			
-			if (verbosity >= 2) {
-				System.out.println("Comparing '" + type1.getSimpleName() + "'");
-			}
-			for (CtType<?> type2 : types2) {
-				// TODO: hard-coded, should be parameterized via blacklist, e.g., "excludedTypeNames" and then
-				//  excludedTypeNames.stream().anyMatch(x -> x.equals(type.getSimpleName()))
-				if (type2.getSimpleName().equals("In") || type2.getSimpleName().equals("Out")) {
-					continue;
-				}
-				
-				comparison.put(type2, new ArrayList<>());
-				
-				Diff typeDiff = comparator.compare(type1, type2);
-				int typeDiffSize = typeDiff.getRootOperations().size();
-				typeDiffSizes.add(Pair.of(type2, typeDiffSize));
-				
-				CtType<?> renamedType2 = renamer2.renameType(type2);
-				Diff renamedTypeDiff = comparator.compare(renamedType1, renamedType2);
-				int renamedTypeDiffSize = renamedTypeDiff.getRootOperations().size();
-				renamedTypeDiffSizes.add(Pair.of(type2, renamedTypeDiffSize));
-				
-				int count2 = countElements(type2);
-				int countDiff = Math.abs(count1 - count2);
-				countDiffs.add(Pair.of(type2, countDiff));
-				
-				int levenDiff = new LevenshteinDistance().apply(type1.getSimpleName(), type2.getSimpleName());
-				levenDiffs.add(Pair.of(type2, levenDiff));
-				
-				if (verbosity >= 2) {
-					System.out.format("  to %25s: typeDiffSize = %3d, renamedTypeDiffSize = %3d, countDiff = %4d, levenDiff = %3d\n", "'" + type2.getSimpleName() + "'", typeDiffSize, renamedTypeDiffSize, countDiff, levenDiff);
-				}
-			}
-			
-			// TODO: comments do not include levenDiff
-			// For each diff list, sort according to the minimum diff and then assign values 1, 2, 3, ... to the
-			// respective AST elements (the various "type2" from above). Afterwards, for each AST element (for each
-			// "type2" from above), we have a normalized sorting. Example:
-			// Comparing 'Exercise1' ("type1")
-			//   to OneDimensionalArray ("type2"), typeDiffSize = 55, renamedTypeDiffSize = 52, countDiff =  11
-			//   to      StoryGenerator ("type2"), typeDiffSize = 70, renamedTypeDiffSize = 63, countDiff =  36
-			//   to      TextConversion ("type2"), typeDiffSize = 58, renamedTypeDiffSize = 59, countDiff =  46
-			//   to       TwoDimensions ("type2"), typeDiffSize = 93, renamedTypeDiffSize = 97, countDiff = 284
-			// This will be mapped to:
-			// Comparing 'Exercise1' ("type1")
-			//   to OneDimensionalArray ("type2"), typeDiffSize = 1, renamedTypeDiffSize = 1, countDiff = 1
-			//   to      StoryGenerator ("type2"), typeDiffSize = 3, renamedTypeDiffSize = 3, countDiff = 2
-			//   to      TextConversion ("type2"), typeDiffSize = 2, renamedTypeDiffSize = 2, countDiff = 3
-			//   to       TwoDimensions ("type2"), typeDiffSize = 4, renamedTypeDiffSize = 4, countDiff = 4
-			sortAndAddNormalized(typeDiffSizes, comparison);
-			sortAndAddNormalized(renamedTypeDiffSizes, comparison);
-			sortAndAddNormalized(countDiffs, comparison);
-			sortAndAddNormalized(levenDiffs, comparison);
-			
-			// With the new, normalized mapping from above, we can now look for the "best" match, where "best" is simply
-			// defined as the minimum product of an AST element's (a "type2"'s) differences. The product (compared to
-			// summing the differences) has the advantage that multiple low values are more heavily rewarded, e.g., if
-			// there are the diffs (1, 1, 4) and (2, 2, 1), the product (4 vs. 9) would still favor the first diff,
-			// whereas the sum (8 vs. 7) would favor the second diff, which probably follows the intuition that the
-			// first diff is likely the matching one ("two best scores vs. probably one outlier"). Of course, this is
-			// just a heuristic and can lead to false results. Example from above:
-			// Comparing 'Exercise1' ("type1")
-			//   to OneDimensionalArray ("type2"), typeDiffSize = 1, renamedTypeDiffSize = 1, countDiff = 1 --> product =  1
-			//   to      StoryGenerator ("type2"), typeDiffSize = 3, renamedTypeDiffSize = 3, countDiff = 2 --> product = 18
-			//   to      TextConversion ("type2"), typeDiffSize = 2, renamedTypeDiffSize = 2, countDiff = 3 --> product = 12
-			//   to       TwoDimensions ("type2"), typeDiffSize = 4, renamedTypeDiffSize = 4, countDiff = 4 --> product = 64
-			// In this case, the 'OneDimensionalArray' AST element ("type2") will be selected as the best match for the
-			// original AST element 'Exercise1' ("type1").
-			CtType<?> matchingType = comparison.entrySet().stream()
-					.map(entry -> Pair.of(entry.getKey(), entry.getValue().stream()
-							.mapToInt(x -> x)
-							.reduce(1, (a, x) -> a * x)))
-					.min(Comparator.comparingInt(Pair::getRight))
-					.map(Pair::getLeft)
-					.orElseThrow();
-			
-			List<Pair<String, Double>> metrics = computeMetrics(type1, matchingType, renamedType1, renamer2.renameType(matchingType));
+			CtType<?> matchingType = findMatchingType(type1, types2, renamer1, renamer2, verbosity);
+			List<Pair<String, Double>> metrics = computeMetrics(type1, matchingType, renamer1.renameType(type1), renamer2.renameType(matchingType));
 			folderComparison.addTypeComparison(new TypeComparison(type1, matchingType, metrics));
 		}
 		
 		return folderComparison;
+	}
+	
+	private static CtType<?> findMatchingType(CtType<?> type1, Iterable<CtType<?>> types2, ASTRenamer renamer1, ASTRenamer renamer2, int verbosity) {
+		AstComparator comparator = new AstComparator();
+		int count1 = countElements(type1);
+		CtType<?> renamedType1 = renamer1.renameType(type1);
+		
+		Map<CtType<?>, List<Integer>> comparison = new HashMap<>();
+		// TODO: currently, these 4 are hard-coded; could make this more generic to allow arbitrary diffs
+		ArrayList<Pair<CtType<?>, Integer>> typeDiffSizes = new ArrayList<>();
+		ArrayList<Pair<CtType<?>, Integer>> renamedTypeDiffSizes = new ArrayList<>();
+		ArrayList<Pair<CtType<?>, Integer>> countDiffs = new ArrayList<>();
+		ArrayList<Pair<CtType<?>, Integer>> levenDiffs = new ArrayList<>();
+		
+		if (verbosity >= 2) {
+			System.out.println("Comparing '" + type1.getSimpleName() + "'");
+		}
+		for (CtType<?> type2 : types2) {
+			// TODO: hard-coded, should be parameterized via blacklist, e.g., "excludedTypeNames" and then
+			//  excludedTypeNames.stream().anyMatch(x -> x.equals(type.getSimpleName()))
+			if (type2.getSimpleName().equals("In") || type2.getSimpleName().equals("Out")) {
+				continue;
+			}
+			
+			comparison.put(type2, new ArrayList<>());
+			
+			Diff typeDiff = comparator.compare(type1, type2);
+			int typeDiffSize = typeDiff.getRootOperations().size();
+			typeDiffSizes.add(Pair.of(type2, typeDiffSize));
+			
+			CtType<?> renamedType2 = renamer2.renameType(type2);
+			Diff renamedTypeDiff = comparator.compare(renamedType1, renamedType2);
+			int renamedTypeDiffSize = renamedTypeDiff.getRootOperations().size();
+			renamedTypeDiffSizes.add(Pair.of(type2, renamedTypeDiffSize));
+			
+			int count2 = countElements(type2);
+			int countDiff = Math.abs(count1 - count2);
+			countDiffs.add(Pair.of(type2, countDiff));
+			
+			int levenDiff = new LevenshteinDistance().apply(type1.getSimpleName(), type2.getSimpleName());
+			levenDiffs.add(Pair.of(type2, levenDiff));
+			
+			if (verbosity >= 2) {
+				System.out.format("  to %25s: typeDiffSize = %3d, renamedTypeDiffSize = %3d, countDiff = %4d, levenDiff = %3d\n", "'" + type2.getSimpleName() + "'", typeDiffSize, renamedTypeDiffSize, countDiff, levenDiff);
+			}
+		}
+		
+		// TODO: comments do not include levenDiff
+		// For each diff list, sort according to the minimum diff and then assign values 1, 2, 3, ... to the
+		// respective AST elements (the various "type2" from above). Afterwards, for each AST element (for each
+		// "type2" from above), we have a normalized sorting. Example:
+		// Comparing 'Exercise1' ("type1")
+		//   to OneDimensionalArray ("type2"), typeDiffSize = 55, renamedTypeDiffSize = 52, countDiff =  11
+		//   to      StoryGenerator ("type2"), typeDiffSize = 70, renamedTypeDiffSize = 63, countDiff =  36
+		//   to      TextConversion ("type2"), typeDiffSize = 58, renamedTypeDiffSize = 59, countDiff =  46
+		//   to       TwoDimensions ("type2"), typeDiffSize = 93, renamedTypeDiffSize = 97, countDiff = 284
+		// This will be mapped to:
+		// Comparing 'Exercise1' ("type1")
+		//   to OneDimensionalArray ("type2"), typeDiffSize = 1, renamedTypeDiffSize = 1, countDiff = 1
+		//   to      StoryGenerator ("type2"), typeDiffSize = 3, renamedTypeDiffSize = 3, countDiff = 2
+		//   to      TextConversion ("type2"), typeDiffSize = 2, renamedTypeDiffSize = 2, countDiff = 3
+		//   to       TwoDimensions ("type2"), typeDiffSize = 4, renamedTypeDiffSize = 4, countDiff = 4
+		sortAndAddNormalized(typeDiffSizes, comparison);
+		sortAndAddNormalized(renamedTypeDiffSizes, comparison);
+		sortAndAddNormalized(countDiffs, comparison);
+		sortAndAddNormalized(levenDiffs, comparison);
+		
+		// With the new, normalized mapping from above, we can now look for the "best" match, where "best" is simply
+		// defined as the minimum product of an AST element's (a "type2"'s) differences. The product (compared to
+		// summing the differences) has the advantage that multiple low values are more heavily rewarded, e.g., if
+		// there are the diffs (1, 1, 4) and (2, 2, 1), the product (4 vs. 9) would still favor the first diff,
+		// whereas the sum (8 vs. 7) would favor the second diff, which probably follows the intuition that the
+		// first diff is likely the matching one ("two best scores vs. probably one outlier"). Of course, this is
+		// just a heuristic and can lead to false results. Example from above:
+		// Comparing 'Exercise1' ("type1")
+		//   to OneDimensionalArray ("type2"), typeDiffSize = 1, renamedTypeDiffSize = 1, countDiff = 1 --> product =  1
+		//   to      StoryGenerator ("type2"), typeDiffSize = 3, renamedTypeDiffSize = 3, countDiff = 2 --> product = 18
+		//   to      TextConversion ("type2"), typeDiffSize = 2, renamedTypeDiffSize = 2, countDiff = 3 --> product = 12
+		//   to       TwoDimensions ("type2"), typeDiffSize = 4, renamedTypeDiffSize = 4, countDiff = 4 --> product = 64
+		// In this case, the 'OneDimensionalArray' AST element ("type2") will be selected as the best match for the
+		// original AST element 'Exercise1' ("type1").
+		return comparison.entrySet().stream()
+				.map(entry -> Pair.of(entry.getKey(), entry.getValue().stream()
+						.mapToInt(x -> x)
+						.reduce(1, (a, x) -> a * x)))
+				.min(Comparator.comparingInt(Pair::getRight))
+				.map(Pair::getLeft)
+				.orElseThrow();
 	}
 	
 	private static void sortAndAddNormalized(ArrayList<Pair<CtType<?>, Integer>> diffs, Map<CtType<?>, List<Integer>> comparison) {
