10aa97d6c6ba01b33f7069ceb7e18449eb5c34ea
Andreas Schörgenhumer <andischoe@gmail.com>
2023-02-02 17:16:45 +0100

    Add javadoc to util method

--- src/main/java/ast/ASTUtil.java
+++ src/main/java/ast/ASTUtil.java
@@ -4,6 +4,13 @@ import spoon.reflect.declaration.CtElement;
 
 public class ASTUtil {
 	
+	/**
+	 * Returns the number of elements of <code>element</code> and all of its children (recursively). The returned value
+	 * is at least 1 (<code>element</code> itself if it does not have any children).
+	 *
+	 * @param element The root element to start counting at
+	 * @return The number of elements of <code>element</code> and all of its children
+	 */
 	public static int countElements(CtElement element) {
 		int count = 1;
 		for (CtElement e : element.getDirectChildren()) {
