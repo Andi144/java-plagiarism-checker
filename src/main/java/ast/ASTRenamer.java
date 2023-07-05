b47c6d61b3e80e1d2e7d25bc15c46371249feef2
Andreas Schörgenhumer <andischoe@gmail.com>
2023-07-05 11:24:49 +0200

    Ensure original is never changed and describe potential renaming problems

--- src/main/java/ast/ASTRenamer.java
+++ src/main/java/ast/ASTRenamer.java
@@ -1,5 +1,6 @@
 package ast;
 
+import org.apache.commons.lang3.tuple.Pair;
 import spoon.Launcher;
 import spoon.refactoring.CtRenameGenericVariableRefactoring;
 import spoon.refactoring.Refactoring;
@@ -64,6 +65,21 @@ public class ASTRenamer {
 	// TODO: maybe put "path" into "rename" method, so the same renaming settings can be used for multiple files, and
 	//  then return the model in "rename"; also, this would mean that we lose the state, so methods like "toString" and
 	//  "getTopLevelTypes" would not make sense afterwards anymore (ast.ASTRenamer is stateless w.r.t. to the renamed models)
+	//
+	// TODO: There is a spoon problem when includeTypeCount is false: If so, then renaming types naturally leads to the same
+	//  template name T. If there are types A, B and C, and A uses B (e.g., as a field), then this only works if the order
+	//  of renaming is A and then directly followed by B, i.e., there must not be any type in between the two. If there
+	//  is, the following happens: A is renamed to T, and then let's say type C is next in the processing order. C is now
+	//  also renamed to T. With this, the unique connection to B (e.g., the field) is apparently lost in spoon, and then
+	//  renaming B (to also T) has no effect on A, i.e., the field B remains in (the renamed) type A. This can easily be
+	//  resolved by setting includeTypeCount to True (and countTypesGlobally also set to True) , but then, the new names will
+	//  (of course) be different and once again  dependent on the processing order (e.g., for A, B, C it would be T1, T2, T3,
+	//  but for A, C, B it would also be T1, T2, T3, i.e., B and C will be named differently albeit the actual same types).
+	//  Both settings are thus not ideal  when calculating the renaming-based metrics afterward, so one has to choose the
+	//  lesser of two evils (whatever that is). Another suboptimal solution would be to make copies of all types before
+	//  the renaming process. This way, all types will be decoupled, which, however, means that, e.g., A's field referencing
+	//  B would stay the same, i.e., it would not be renamed at all anymore (regardless of the type processing order)
+	// String path should be Path path (consistency)
 	public ASTRenamer(
 			String path, Set<String> excludedTypeNames, boolean keepComments,
 			boolean includeTypeCount, boolean includeFieldCount, boolean includeMethodCount, boolean includeParameterCount, boolean includeLocalVariableCount,
@@ -87,15 +103,21 @@ public class ASTRenamer {
 		parameterData = new RenamingData(includeParameterCount, countParametersGlobally, parameterTemplate);
 		localVariableData = new RenamingData(includeLocalVariableCount, countLocalVariablesGlobally, localVariableTemplate);
 		
-		// For some reason, copying the types and then renaming them does not work properly (some fields are not
-		// correctly renamed), so rename the original types in-place and simply keep the (non-renamed) copies as
-		// original. This can only be done once, so perform the operation right here in the constructor
-		Collection<CtType<?>> allTypes = model.getAllTypes();
+		// Copying decouples the type from the original types (which must also have been copied, there is an effect otherwise!)
+		boolean decouple = true; // TODO: parameterize
+		List<Pair<CtType<?>, CtType<?>>> originalAndToBeRenamed = new ArrayList<>();
+		for (CtType<?> ctType : model.getAllTypes()) {
+			if (!excludedTypeNames.contains(ctType.getSimpleName())) {
+				// Make sure to copy all original types before to avoid any side effect when changing the toBeRenamed type
+				originalAndToBeRenamed.add(Pair.of(ctType.clone(), decouple ? ctType.clone() : ctType));
+			}
+		}
 		List<Type> originalAndRenamed = new ArrayList<>();
-		allTypes.stream().filter(ctType -> !excludedTypeNames.contains(ctType.getSimpleName())).forEach(ctType -> {
-			CtType<?> copy = ctType.clone();
-			renameType(ctType); // in-place renaming of "ctType" ("copy" retains original data)
-			originalAndRenamed.add(new Type(copy, ctType));
+		originalAndToBeRenamed.forEach(pair -> {
+			CtType<?> original = pair.getLeft();
+			CtType<?> toBeRenamed = pair.getRight();
+			renameType(toBeRenamed);
+			originalAndRenamed.add(new Type(original, toBeRenamed));
 		});
 		types = Collections.unmodifiableList(originalAndRenamed);
 	}
